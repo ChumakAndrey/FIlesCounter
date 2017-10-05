f83000219bf79f74bfc9ea619e6f049ddd52f935
Андрей <chumakandrey7@gmail.com>
2017-10-05 13:25:20 +0300

    Ready to check

--- src/utils/FilesCounter.java
+++ src/utils/FilesCounter.java
@@ -11,12 +11,6 @@ import java.util.List;
  */
 public final class FilesCounter {
 
-    /**
-     * Рекурсивный метод, который считает количество файлов в папке.
-     * Если в заданной директории есть вложенные папки, метод вызывает сам себя.
-     *
-     * @param path полный путь к директории, в которой нужен подсчет файлов.
-     **/
     public static long amountOfFiles(String path) {
         long countedFiles=0;
         File folder = new File(path);
